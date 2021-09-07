339c90bc1294096cab5dbc53cf75749b412283d8
kuzichevapd <kuzicheva.pd@edu.spbstu.ru>
2021-09-07 21:41:42 +0300

    строка 68 работает строка 77 работает

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -74,7 +74,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = seconds + minutes*60 
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (vershoks + arshins*16 + sagenes*48) * 0.04445
 
 /**
  * Тривиальная (1 балл)
